b42ebece3f32672a934bf53904a6bdd21c9283f4
FafnirNithhoggr <felixmonteiroo.o@gmail.com>
2022-12-13 21:08:17 +0000

    i want regex.h to fall over and die

--- src/parser.c
+++ src/parser.c
@@ -14,69 +14,15 @@ char *strrev(char *str){
       return str;
 }
 
+ //           if(sscanf(arg,"ftp://%99[^:/ @]/%99[^: @\n]", args->host, args->path)!=)
+
 // --------------- ARGUMENT PARSER -----------------
 int argParser(struct Args *args, char *arg){
         
       printf("parsing arguments\n");
-            
-      // ------ check if input is valid
-      regex_t regex;
-      int comp_status;
-      comp_status = regcomp(&regex,"^(ftp:/)?/?(([^:/ @]+):([^:/ @]+)@)?([^:/ @]+)/([^: @]+)$",0);
-      if(comp_status!=0){
-            printf("regular expression compilation error\n");
-            return -1;
-      }
-      
-      int argument_valid;
-      argument_valid = regexec(&regex, arg, 0, NULL, 0);
-      if(argument_valid!=0){
-            printf("Incorrect input format\n");
-            return -1;
-      }
 
-      // ------ if input does not have login info
+      if(sscanf(arg,"ftp://%99[^:/ @]:%99[^:/ @]@%99[^:/ @]/%99[^: @\n]", args->user, args->password, args->host, args->path)==4){
 
-      comp_status = regcomp(&regex,"^(ftp:/)?/?([^:/ @]+)/([^: @]+)$",0);
-      if(comp_status!=0){
-            printf("regular expression compilation error\n");
-            return -1;
-      }
-      
-      argument_valid = regexec(&regex, arg, 0, NULL, 0);
-      if(argument_valid!=0){
-            printf("Incorrect input format\n");
-            return -1;
-      }else{
-
-            sscanf(arg,"ftp://%99[^:/ @]/%99[^: @\n]", args->host, args->path);
-            char rev[BUFF_SIZE];
-            strcpy(rev, args->path);
-            strcpy(rev, strrev(rev));
-            sscanf(rev,"%99[^:/ @]/",rev);
-            strcpy(rev, strrev(rev));
-            strcpy(args->name, rev);
-            printf("host = \"%s\"\n", args->host);
-            printf("path = \"%s\"\n", args->path);
-            printf("name = \"%s\"\n", args->name);
-
-      }
-
-      // ------ if input does not have login info
-
-      comp_status = regcomp(&regex,"^(ftp:/)?/?(([^:/ @]+):([^:/ @]+)@)([^:/ @]+)/([^: @]+)$",0);
-      if(comp_status!=0){
-            printf("regular expression compilation error\n");
-            return -1;
-      }
-      
-      argument_valid = regexec(&regex, arg, 0, NULL, 0);
-      if(argument_valid!=0){
-            printf("Incorrect input format\n");
-            return -1;
-      }else{
-
-            sscanf(arg,"ftp://%99[^:/ @]:%99[^:/ @]@%99[^:/ @]/%99[^: @\n]", args->user, args->password, args->host, args->path);
             char rev[BUFF_SIZE];
             strcpy(rev, args->path);
             strcpy(rev, strrev(rev));
@@ -89,9 +35,22 @@ int argParser(struct Args *args, char *arg){
             printf("path = \"%s\"\n", args->path);
             printf("name = \"%s\"\n", args->name);
 
+      }else if(sscanf(arg,"ftp://%99[^:/ @]/%99[^: @\n]", args->host, args->path)==2){
+
+            char rev[BUFF_SIZE];
+            strcpy(rev, args->path);
+            strcpy(rev, strrev(rev));
+            sscanf(rev,"%99[^:/ @]/",rev);
+            strcpy(rev, strrev(rev));
+            strcpy(args->name, rev);
+            printf("host = \"%s\"\n", args->host);
+            printf("path = \"%s\"\n", args->path);
+            printf("name = \"%s\"\n", args->name);
+
+      }else{
+            printf("Incorrect input format\n");
+            return -1;
       }
-
-
 
     return 0;
 }
