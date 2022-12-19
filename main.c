d925bd16eddf3c5b31428ed1dbeac9ee3f022f9c
Johnny-droid <76396827+Johnny-droid@users.noreply.github.com>
2022-12-19 15:42:42 +0000

    Update main.c

--- main.c
+++ main.c
@@ -12,6 +12,7 @@ int main(int argc, char **argv) {
 
     if (argc != 2) {
         printf("use: download ftp://[<user>:<password>@]<host>/<url-path>\n");
+        return -1;
     }
 
     // TODO: Instead of this, we need to make a parser
